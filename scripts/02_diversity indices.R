2cc5b2022c2c05e965549a4588fef0d528964931
Tia (they/them) <malloff.tia@gmail.com>
2021-11-24 20:08:59 -0800

    did paired t-test and made box plot to show paired data

--- scripts/02_diversity indices.R
+++ scripts/02_diversity indices.R
@@ -1,6 +1,6 @@
 ## BIOL 427 Calculating Diversity Indicies and statistical tests on bird counts at an old growth and new growth site
 ## Author: Juniper Malloff 
-# Last updated: 2021/11/22
+# Last updated: 2021/11/24
 
 library(tidyverse)
 library(vegan)
@@ -33,12 +33,22 @@ birdcounts_simp_old <- birdcounts_simp %>%
 birdcounts_simp_new <- birdcounts_simp %>%
   subset(site == "New")
 
+## subtract old from new, run paired T
 t.test(birdcounts_simp_old$simpson, birdcounts_simp_new$simpson)
 
+t.test(birdcounts_simp_new$simpson, birdcounts_simp_old$simpson, paired = TRUE)
 
-ggplot(birdcounts_simp) + geom_boxplot(mapping = aes(site, simpson)) 
+ggplot(birdcounts_simp) + geom_col(aes(date, simpson, fill = site), position = "dodge") + theme_classic()
 
-specaccum(birdcounts_simp_new[8:33], method = "exact")
+ggplot(birdcounts_simp) + 
+  geom_boxplot(aes(site, simpson)) +
+  geom_point(aes(site, simpson)) + 
+  geom_line(aes(site, simpson, group = date))+
+  theme_classic()
 
-plot
+  
+
+
+
+
 
